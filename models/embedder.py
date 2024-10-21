e830d82900c9ef6de4de36e39f932e22186ef7d3
Kevin Joshua <91450245+Crack-Fettuccini@users.noreply.github.com>
2024-10-21 06:37:59 +0530

    check for weights

--- models/embedder.py
+++ models/embedder.py
@@ -2,9 +2,19 @@ import torch
 import torch.nn as nn
 
 class TokenEmbedder(nn.Module):
-    def __init__(self, vocab_size, embedding_dim):
+    def __init__(self, vocab_size, embedding_dim, pretrained_weights=None, freeze=False, dropout=0.1):
         super(TokenEmbedder, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        
+        if pretrained_weights is not None:
+            self.embedding.weight = nn.Parameter(pretrained_weights)
+        
+        if freeze:
+            self.embedding.weight.requires_grad = False
+        
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, tokens):
-        return self.embedding(tokens)
+        embeddings = self.embedding(tokens)
+        return self.dropout(embeddings)
+
